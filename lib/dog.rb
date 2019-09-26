294937f4a7e4549050ab907db4d609a6e9e1d254
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-26 17:39:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
     dog = DB[:conn].execute(sql,name,breed).map do |row|
       self.new_from_db(row)
       end.first
-      binding.pry
+      # binding.pry
     if dog != nil
       dog
     else
