794962e9a7552cccda4d214b78fda6b212d2db54
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-26 17:15:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,9 +62,7 @@ class Dog
 
   def self.find_or_create_by(name:, breed:)
     # find_by name/breed
-    sql = <<-SQL
-    SELECT * FROM dogs WHERE id = ? LIMIT 1
-    SQL
+    if self.find_by_name
 
   end
 
