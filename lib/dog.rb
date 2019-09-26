ab27f684662aea10645f9ade4018bb6faff48f40
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-26 17:56:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,12 +84,12 @@ class Dog
       self.new_from_db(row)
     end.first
   end
-  #
-  # def update
-  #   sql = <<-SQL
-  #   UPDATE dogs SET name = ?, breed = ?, id = ?
-  #   SQL
-  #   DB[:conn].execute(sql, self.name, self.breed, self.id)
-  # end
+  
+  def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ?, id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 end
